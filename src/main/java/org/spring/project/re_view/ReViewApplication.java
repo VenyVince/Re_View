5a65ff590ad30a559c077e093e9e9217dd12b8ba
Veny <redguy0814@gmail.com>
2025-10-21 11:31:43 +0900

    init

---
+++ src/main/java/org/spring/project/re_view/ReViewApplication.java
@@ -0,0 +1,13 @@
+package org.spring.project.re_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReViewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/project/re_view/ReViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.project.re_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
