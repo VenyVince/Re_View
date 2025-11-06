1c2f35ca69aca5ea24b2c11def95e61e73dccd40
abluehour <abluehour@gmail.com>
2025-11-06 17:43:15 +0900

    chore: security 오류 해결

--- src/main/java/org/spring/project/re_view/ReViewApplication.java
+++ src/main/java/org/spring/project/re_view/ReViewApplication.java
@@ -1,9 +1,11 @@
-package com.review.shop;
+package org.spring.project.re_view;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.review.shop")
+@MapperScan("com.review.shop.repository")
 public class ReViewApplication {
 
     public static void main(String[] args) {
