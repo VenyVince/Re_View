8a158129b8eb6df5c36d4a90763b31475fc796aa
VenyVince <redguy0814@gmail.com>
2025-11-06 18:19:19 +0900

    conflict : rebase중 나온 충돌 해결중 13트

---
+++ src/main/java/com/review/shop/controller/Search/CommonSearchAPI.java
@@ -0,0 +1,4 @@
+package com.review.shop.controller.search;
+
+public class CommonSearchAPI {
+}
---
+++ src/main/java/com/review/shop/dto/search/CommonProductDTO.java
@@ -0,0 +1,12 @@
+package com.review.shop.dto.search;
+
+
+
+public class CommonProductDTO {
+    private String prd_Name;
+    private String prd_brand;
+    private int price;
+    private float rating;
+    
+    //getter setter 구현
+}
