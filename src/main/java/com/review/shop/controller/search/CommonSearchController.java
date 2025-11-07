5d8e5e328761c2d8ea9dc73564863f2a23c026e6
VenyVince <redguy0814@gmail.com>
2025-11-07 13:24:14 +0900

    conflit: merge conflict resolve

--- src/main/java/com/review/shop/controller/search/CommonSearchController.java
+++ src/main/java/com/review/shop/controller/search/CommonSearchController.java
@@ -1,7 +1,10 @@
 package com.review.shop.controller.search;
 
-import com.review.shop.dto.search.CommonSearchResponse;
+import com.review.shop.dto.search.CommonSearchDTO;
 import com.review.shop.service.search.CommonSearchService;
+import com.review.shop.service.search.CommonSearchService;
+import org.apache.coyote.BadRequestException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +17,17 @@ public class CommonSearchController {
     private CommonSearchService searchService;
 
     @GetMapping
-    public ResponseEntity<CommonSearchResponse> search(@RequestParam String keyword) {
-        CommonSearchResponse response = searchService.search(keyword);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<CommonSearchDTO> search(
+            @RequestParam(required = false, defaultValue = "") String keyword,
+            @RequestParam(required = false, defaultValue = "latest") String sort, //미구현
+            @RequestParam(required = false, defaultValue = "") String filter //미구현
+    ) throws BadRequestException {
+        if(keyword.length()<2){
+            throw new BadRequestException("검색어는 2글자 이상부터입니다.");
+        }
+
+        return ResponseEntity.ok(searchService.search(keyword));
+//        CommonSearchResponse response = searchService.search(keyword);
+//        return ResponseEntity.ok(response);
     }
 }
