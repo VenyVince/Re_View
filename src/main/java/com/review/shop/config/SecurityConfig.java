2bbfd45db5d13439beb3571c28b6fcdd440a7a10
LeeJaeBin <binjae2@gmail.com>
2025-11-07 10:49:48 +0900

    feat: 로그인, 회원가입, 로그아웃 기능 추가
    # Conflicts:
    #	src/main/java/com/review/shop/config/SecurityConfig.java

--- src/main/java/com/review/shop/config/SecurityConfig.java
+++ src/main/java/com/review/shop/config/SecurityConfig.java
@@ -1,23 +1,58 @@
 package com.review.shop.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-// Security Config test용으로 임시로 permit ALl 걸어놓음. 테스트용으로만 확인하고 나중에 배포할 때랑 로그인 구현되고 나서는 삭제.
-// 비밀번호랑 아이디는  properties에서 확인.
+
 @Configuration
-@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    //사용자 인증 처리 Bean 주입
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    //암호화 용 Bean 주입
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                //현재 restful api 방식이므로 formLogin, httpBasic 비활성화(기존은 html form 로그인 방식)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+
+                // 주의 !!!! CSRF 비활성화 되어있음
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화 (개발 기간동안)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                // 로그아웃 설정
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout")
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+
+                        })
+
+        );
+
+
         return http.build();
     }
 }
---
+++ src/main/java/com/review/shop/service/UserService.java
@@ -0,0 +1,65 @@
+package com.review.shop.service;
+
+import com.review.shop.mapper.UserMapper;
+import com.review.shop.model.UserInfoDto;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    // 회원가입 로직 구현, DB 결과에 따른 예외 처리
+    public void registerUser(UserInfoDto userDTO) {
+
+        // 중복 ID 체크 추가
+        if (userMapper.findUserById(userDTO.getId()) != null) {
+            throw new IllegalStateException("이미 존재하는 아이디입니다.");
+        }
+        // 컨트롤러 부터 받은 dto 비밀번호를 암호화
+        String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+
+        UserInfoDto encodedUser = new UserInfoDto(
+                userDTO.getId(),
+                encodedPassword,
+                userDTO.getName(),
+                userDTO.getEmail(),
+                userDTO.getNickname(),
+                userDTO.getPhoneNumber(),
+                userDTO.getBaumannId(),
+                userDTO.getRole()
+        );
+
+        int affected = userMapper.insertUser(encodedUser);
+
+        if (affected != 1) {
+            throw new IllegalStateException("회원가입에 실패했습니다.");
+        }
+    }
+
+    // AuthenticationManager가 UserDetails을 받기위한 메소드
+    // 로그인 데이터와 UserDetails의 데이터를 비교하여 인증 처리
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+
+        UserInfoDto user = userMapper.findUserById(id);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + id);
+        }
+
+        return User.builder()
+                .username(user.getId())
+                .password(user.getPassword()) // DB에 저장된 암호화된 비밀번호
+                .roles(user.getRole()) // 사용자의 권한
+                .build();
+    }
+}
