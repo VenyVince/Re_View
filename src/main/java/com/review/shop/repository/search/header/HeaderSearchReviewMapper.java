5270f114bc6d6516206f5e6cb9576481994cbbc2
VenyVince <redguy0814@gmail.com>
2025-11-07 14:45:52 +0900

    feat: add "sort"

---
+++ src/main/java/com/review/shop/repository/search/header/HeaderSearchReviewMapper.java
@@ -0,0 +1,12 @@
+package com.review.shop.repository.search.header;
+
+import com.review.shop.dto.search.header.HeaderSearchReviewDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface HeaderSearchReviewMapper {
+    List<HeaderSearchReviewDTO> searchReviews(@Param("keyword") String keyword, @Param("sort") String sort);
+}
