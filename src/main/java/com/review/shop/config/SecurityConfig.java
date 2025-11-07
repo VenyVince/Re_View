026db66ea3f7f00ff782558242838980375f5fe2
VenyVince <redguy0814@gmail.com>
2025-11-07 11:29:03 +0900

    fix: error resolving

--- src/main/java/com/review/shop/config/SecurityConfig.java
+++ src/main/java/com/review/shop/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.review.shop.config;
 
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
