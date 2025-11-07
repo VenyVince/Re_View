b2ad74cdae80c30080b615ca7dbc04aa473a38ce
VenyVince <redguy0814@gmail.com>
2025-11-07 11:29:03 +0900

    fix: error resolving

--- src/main/java/com/review/shop/config/SecurityConfig.java
+++ src/main/java/com/review/shop/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@AllArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
     //사용자 인증 처리 Bean 주입
     @Bean
