7d4cbc2504d57092b3ac6937fd400e3488a51cfc
VenyVince <redguy0814@gmail.com>
2025-11-07 13:24:14 +0900

    conflit: merge conflict resolve

--- src/main/java/com/review/shop/config/ExceptionHandlers.java
+++ src/main/java/com/review/shop/config/ExceptionHandlers.java
@@ -5,7 +5,6 @@ import com.review.shop.exception.ResourceNotFountException;
 import com.review.shop.exception.WrongRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
