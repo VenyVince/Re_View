ea48bd99670ba3e409a889bdc4cf93f5004ee5c4
Veny <redguy0814@gmail.com>
2025-11-14 23:49:45 +0900

    temp: MyPage Review 파트 수정 중

--- src/main/java/com/review/shop/controller/review/MyPageReviewController.java
+++ src/main/java/com/review/shop/controller/review/MyPageReviewController.java
@@ -2,13 +2,15 @@ package com.review.shop.controller.review;
 
 import com.review.shop.Util.Security_Util;
 import com.review.shop.dto.review.MyPageReviewResponseDTO;
-import com.review.shop.dto.search.ProductReview_SearchResponseDTO;
 import com.review.shop.service.review.MyPageReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users/reviews/search")
@@ -25,6 +27,6 @@ public class MyPageReviewController {
             @RequestParam(required = false, defaultValue = "0") float filter_rating
     ){
         int user_id = security_util.getCurrentUserId();
-        return ResponseEntity.ok(mypagereviewService.(user_id, keyword, sort, filter_rating));
+        return ResponseEntity.ok(mypagereviewService.search(user_id, keyword, sort, filter_rating));
     }
 }
