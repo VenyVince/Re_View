b05cbfae04adf202ffc863bb7628841364752c81
LeeJaeBin <binjae2@gmail.com>
2025-11-18 11:46:09 +0900

    chore : 어드민 상품 조회에 이미지 추가

---
+++ src/main/java/com/review/shop/controller/orders/OrderController.java
@@ -0,0 +1,9 @@
+package com.review.shop.controller.orders;
+
+
+// 프론트에서는 prdid, quantity를 리스트로 reqeust에 넣어서 보낼것임
+// ex) { "orders": [ {"prdid":1, "quantity":2}, {"prdid":2, "quantity":1} ] }
+// 나머지 배송지나 포인트같은건 세션조회를 통해 백엔드에서 가져올것임
+public class OrderController {
+
+}
