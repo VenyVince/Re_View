ca17f9c3281ded369dd3392083fb651cc319832c
ksy809 <ss741219@daum.net>
2025-11-19 16:40:13 +0900

    feat : 상품 등록 기능 구현 및 API 연동 완료

--- View/src/api/axiosClient.js
+++ View/src/api/axiosClient.js
@@ -6,5 +6,14 @@ const axiosClient = axios.create({
     withCredentials: true,
 });
 
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");   // 로그인 시 저장된 토큰
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+
 
 export default axiosClient;
