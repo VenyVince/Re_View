4e48e7884a7c02f94cf060ddaaa3e785d8dcb8ac
VenyVince <redguy0814@gmail.com>
2025-11-19 17:57:28 +0900

    feat:
    imageupload컨트롤러 구현(admin, user모두에서 접근할 수 있음, 내부적으로 admin인지 user인지 판단해서 배당)

--- src/main/java/com/review/shop/dto/review/CreateReviewRequest.java
+++ src/main/java/com/review/shop/dto/review/CreateReviewRequest.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateReviewRequest {
     private String content;      // 리뷰 내용 (필수)
     private double rating;       // 평점 1~5 (필수)
+    private List<String> ImageUrls;
 }
--- src/main/java/com/review/shop/service/userinfo/user_related/PointService.java
+++ src/main/java/com/review/shop/service/userinfo/user_related/PointService.java
@@ -1,10 +1,8 @@
 package com.review.shop.service.userinfo.user_related;
 
-import com.review.shop.dto.userinfo.user_related.Point.PointHistoryDTO;
 import com.review.shop.dto.userinfo.user_related.Point.PointResponseDTO;
 import com.review.shop.exception.DatabaseException;
 import com.review.shop.exception.ResourceNotFoundException;
-import com.review.shop.exception.WrongRequestException;
 import com.review.shop.repository.userinfo.user_related.PointMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -42,48 +40,46 @@ public class PointService {
         }
     }
 
-    // 2. 리뷰 작성 보상 적립
-    public void addReviewPoint(int user_id, int review_id, int amount) {
-        try {
-            if (pointMapper.existsReviewPoint(review_id)) return;
+//    // 2. 리뷰 작성 보상 적립
+//    public void addReviewPoint(int user_id, int review_id, int amount) {
+//        try {
+//            if (pointMapper.existsReviewPoint(review_id)) return;
+//
+//            PointHistoryDTO dto = new PointHistoryDTO();
+//            dto.setUser_id(user_id);
+//            dto.setAmount(amount);
+//            dto.setType("EARN");
+//            dto.setDescription("리뷰 작성 보상");
+//
+//            pointMapper.aboutPoint(dto);
+//            pointMapper.insertReviewPointReference(review_id, dto.getUser_id());
+//
+//        } catch (WrongRequestException e) {
+//            throw e;
+//        } catch (Exception e) {
+//            throw new DatabaseException("리뷰 작성 포인트 적립 중 DB 오류가 발생했습니다.", e);
+//        }
+//    }
 
-            PointHistoryDTO dto = new PointHistoryDTO();
-            dto.setUser_id(user_id);
-            dto.setAmount(amount);
-            dto.setType("EARN");
-            dto.setDescription("리뷰 작성 보상");
-
-            pointMapper.aboutPoint(dto);
-            pointMapper.insertReviewPointReference(review_id, dto.getUser_id());
-        } catch (WrongRequestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new DatabaseException("리뷰 작성 포인트 적립 중 DB 오류가 발생했습니다.", e);
-        }
-    }
-
-    // 3. 베스트 리뷰 선정 보상 적립
-    public void addBestReviewPoint(int user_id, int review_id, int amount, boolean is_selected) {
-        if (!is_selected) return;
-
-        try {
-            if (pointMapper.existsBestReviewPoint(review_id)) return;
-
-            PointHistoryDTO dto = new PointHistoryDTO();
-            dto.setUser_id(user_id);
-            dto.setAmount(amount);
-            dto.setType("EARN");
-            dto.setDescription("베스트 리뷰 선정 보상");
-
-            pointMapper.aboutPoint(dto);
-            pointMapper.insertBestReviewPointReference(review_id, dto.getUser_id());
-        } catch (WrongRequestException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new DatabaseException("베스트 리뷰 포인트 적립 중 DB 오류가 발생했습니다.", e);
-        }
-    }
-
-
-
+//    // 3. 베스트 리뷰 선정 보상 적립
+//    public void addBestReviewPoint(int user_id, int review_id, int amount, boolean is_selected) {
+//        if (!is_selected) return;
+//
+//        try {
+//            if (pointMapper.existsBestReviewPoint(review_id)) return;
+//
+//            PointHistoryDTO dto = new PointHistoryDTO();
+//            dto.setUser_id(user_id);
+//            dto.setAmount(amount);
+//            dto.setType("EARN");
+//            dto.setDescription("베스트 리뷰 선정 보상");
+//
+//            pointMapper.aboutPoint(dto);
+//            pointMapper.insertBestReviewPointReference(review_id, dto.getUser_id());
+//        } catch (WrongRequestException e) {
+//            throw e;
+//        } catch (Exception e) {
+//            throw new DatabaseException("베스트 리뷰 포인트 적립 중 DB 오류가 발생했습니다.", e);
+//        }
+//    }
 }
