5ea99b459bcb9560844733cb5c559ea3455e1c31
LeeJaeBin <binjae2@gmail.com>
2025-12-01 15:46:11 +0900

    chore : 추천부분 swagger 수정

--- src/main/java/com/review/shop/controller/recommendations/RecommendationsController.java
+++ src/main/java/com/review/shop/controller/recommendations/RecommendationsController.java
@@ -26,10 +26,10 @@ public class RecommendationsController {
 
     private final RecommendationsService recommendationsService;
 
-    @Operation(summary = "추천 상품 조회", description = "로그인한 사용자의 바우만 유형을 기반으로 추천 상품을 조회합니다.")
+    @Operation(summary = "추천 상품과 리뷰 조회", description = "로그인한 사용자의 바우만 유형을 기반으로 추천 상품을 조회합니다.")
     @PostMapping("/api/recommendations/{type}")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "추천 상품 조회 성공",
+            @ApiResponse(responseCode = "200", description = "추천 상품과 리뷰 조회 성공",
                     content = @Content(schema = @Schema(implementation = RecommendationResponseDTO.class))),
             @ApiResponse(responseCode = "400", description = "백엔드 오류",
                     content = @Content(schema = @Schema(implementation = String.class)))
